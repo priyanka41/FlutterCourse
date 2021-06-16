a6a0fe5bbbb187b26458632f04d00803a1671ede
Priyanka Poudel <priyankapaudel20@gmail.com>
2021-06-15 17:41:58 -0700

    day2 completed

---
+++ lib/homepage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+ 
+
+  @override
+  Widget build(BuildContext context) {
+     int days = 30;
+    String name = 'Priyanka';
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Catalog App'),
+      ),
+        body: Center(
+          child: Container(
+            child: Text('welcome to $days days flutter course by $name'),
+          ),
+        ),
+        drawer: Drawer(),
+      );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,20 @@
+import 'package:design1/homepage.dart';
+import 'package:flutter/material.dart';
 
 
+void main(){
+  runApp(MyAApp());
+}
+
+class MyAApp extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+   
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
